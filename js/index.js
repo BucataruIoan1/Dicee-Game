726b40ec1b06d522d5215925cfeb18a86ff8868a
Bucataru Ioan <bucataruioan1@gmail.com>
2022-02-19 13:46:06 +0200

    Add users and check their values

--- js/index.js
+++ js/index.js
@@ -1,79 +1,77 @@
-
 let buttonElement = document.querySelector('.roll');
-buttonElement.addEventListener('click', function rollDice () {
+buttonElement.addEventListener('click', function rollDice() {
     let value = Math.floor((Math.random() * 6) + 1);
 
     if (value === 1) {
-        addRemoveClasses();
+        addRemoveClasses('face', 'face1');
         document.querySelectorAll('img')[0].classList.add('face1');
     }
     else if (value === 2) {
-        addRemoveClasses();
+        addRemoveClasses('face', 'face1');
         document.querySelectorAll('img')[1].classList.add('face1');
     }
     else if (value === 3) {
-        addRemoveClasses();
+        addRemoveClasses('face', 'face1');
         document.querySelectorAll('img')[2].classList.add('face1');
     }
     else if (value === 4) {
-        addRemoveClasses();
+        addRemoveClasses('face', 'face1');
         document.querySelectorAll('img')[3].classList.add('face1');
     }
     else if (value === 5) {
-        addRemoveClasses();
+        addRemoveClasses('face', 'face1');
         document.querySelectorAll('img')[4].classList.add('face1');
     }
     else if (value === 6) {
-        addRemoveClasses();
+        addRemoveClasses('face', 'face1');
         document.querySelectorAll('img')[5].classList.add('face1');
     }
-     
-});
 
-let buttonElement2 = document.querySelector('.roll');
-buttonElement2.addEventListener('click', function rollDice2 () {
     let value2 = Math.floor((Math.random() * 6) + 1);
 
     if (value2 === 1) {
-        addRemoveClasses2();
+        addRemoveClasses('face0', 'face7');
         document.querySelectorAll('img')[6].classList.add('face7');
     }
     else if (value2 === 2) {
-        addRemoveClasses2();
+        addRemoveClasses('face0', 'face7');
         document.querySelectorAll('img')[7].classList.add('face7');
     }
     else if (value2 === 3) {
-        addRemoveClasses2();
+        addRemoveClasses('face0', 'face7');
         document.querySelectorAll('img')[8].classList.add('face7');
     }
     else if (value2 === 4) {
-        addRemoveClasses2();
+        addRemoveClasses('face0', 'face7');
         document.querySelectorAll('img')[9].classList.add('face7');
     }
     else if (value2 === 5) {
-        addRemoveClasses2();
+        addRemoveClasses('face0', 'face7');
         document.querySelectorAll('img')[10].classList.add('face7');
     }
     else if (value2 === 6) {
-        addRemoveClasses2();
+        addRemoveClasses('face0', 'face7');
         document.querySelectorAll('img')[11].classList.add('face7');
     }
+
+    if (value > value2) {
+        document.querySelector('h2').innerHTML = 'Player 1 wins 🥇';
+    }
+    else if (value < value2) {
+        document.querySelector('h2').innerHTML = 'Player 2 wins 🥇';
+    }
+    else {
+        document.querySelector('h2').innerHTML = 'Draw!';
+    }
+
 });
 
-function addRemoveClasses () {
+function addRemoveClasses (addClass, removeClass) {
     for (let i = 0; i < document.querySelectorAll('img').length; i++) {
-        document.querySelectorAll('img')[i].classList.add('face');
+        document.querySelectorAll('img')[i].classList.add(addClass);
     }
     for (let i = 0; i < document.querySelectorAll('img').length; i++) {
-        document.querySelectorAll('img')[i].classList.remove('face1');
+        document.querySelectorAll('img')[i].classList.remove(removeClass);
     }
 }
 
-function addRemoveClasses2 () {
-    for (let i = 0; i < document.querySelectorAll('img').length; i++) {
-        document.querySelectorAll('img')[i].classList.add('face0');
-    }
-    for (let i = 0; i < document.querySelectorAll('img').length; i++) {
-        document.querySelectorAll('img')[i].classList.remove('face7');
-    }
-}
