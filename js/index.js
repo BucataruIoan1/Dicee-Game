96dacb74792ac6733dc4c6bb34f5208ca42f3888
Bucataru Ioan <bucataruioan1@gmail.com>
2022-02-28 20:05:24 +0200

    Add jQuery to Project

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Target the button element
 
-let buttonElement = document.querySelector('.roll');
+let buttonElement = $('.roll')[0];
 
 // Add a Event Listener (click) and a function who choose a dice face between 1-6
 
@@ -12,27 +12,27 @@ buttonElement.addEventListener('click', function rollDice() {
 
     if (value === 1) {
         addRemoveClasses('face', 'face1');
-        document.querySelectorAll('img')[0].classList.add('face1');
+        $('img')[0].classList.add('face1');
     }
     else if (value === 2) {
         addRemoveClasses('face', 'face1');
-        document.querySelectorAll('img')[1].classList.add('face1');
+        $('img')[1].classList.add('face1');
     }
     else if (value === 3) {
         addRemoveClasses('face', 'face1');
-        document.querySelectorAll('img')[2].classList.add('face1');
+        $('img')[2].classList.add('face1');
     }
     else if (value === 4) {
         addRemoveClasses('face', 'face1');
-        document.querySelectorAll('img')[3].classList.add('face1');
+        $('img')[3].classList.add('face1');
     }
     else if (value === 5) {
         addRemoveClasses('face', 'face1');
-        document.querySelectorAll('img')[4].classList.add('face1');
+        $('img')[4].classList.add('face1');
     }
     else if (value === 6) {
         addRemoveClasses('face', 'face1');
-        document.querySelectorAll('img')[5].classList.add('face1');
+        $('img')[5].classList.add('face1');
     }
 
     // Get a random number value 2
@@ -41,37 +41,37 @@ buttonElement.addEventListener('click', function rollDice() {
 
     if (value2 === 1) {
         addRemoveClasses('face0', 'face7');
-        document.querySelectorAll('img')[6].classList.add('face7');
+        $('img')[6].classList.add('face7');
     }
     else if (value2 === 2) {
         addRemoveClasses('face0', 'face7');
-        document.querySelectorAll('img')[7].classList.add('face7');
+        $('img')[7].classList.add('face7');
     }
     else if (value2 === 3) {
         addRemoveClasses('face0', 'face7');
-        document.querySelectorAll('img')[8].classList.add('face7');
+        $('img')[8].classList.add('face7');
     }
     else if (value2 === 4) {
         addRemoveClasses('face0', 'face7');
-        document.querySelectorAll('img')[9].classList.add('face7');
+        $('img')[9].classList.add('face7');
     }
     else if (value2 === 5) {
         addRemoveClasses('face0', 'face7');
-        document.querySelectorAll('img')[10].classList.add('face7');
+        $('img')[10].classList.add('face7');
     }
     else if (value2 === 6) {
         addRemoveClasses('face0', 'face7');
-        document.querySelectorAll('img')[11].classList.add('face7');
+        $('img')[11].classList.add('face7');
     }
 
     if (value > value2) {
-        document.querySelector('h2').innerHTML = 'Player 1 wins 🥇';
+        $('h2')[0].innerHTML = 'Player 1 wins 🥇';
     }
     else if (value < value2) {
-        document.querySelector('h2').innerHTML = 'Player 2 wins 🥇';
+        $('h2')[0].innerHTML = 'Player 2 wins 🥇';
     }
     else {
-        document.querySelector('h2').innerHTML = 'Draw!';
+        $('h2')[0].innerHTML = 'Draw!';
     }
 
 });
@@ -79,11 +79,11 @@ buttonElement.addEventListener('click', function rollDice() {
 // Add and Delete Classes
 
 function addRemoveClasses (addClass, removeClass) {
-    for (let i = 0; i < document.querySelectorAll('img').length; i++) {
-        document.querySelectorAll('img')[i].classList.add(addClass);
+    for (let i = 0; i < $('img').length; i++) {
+        $('img')[i].classList.add(addClass);
     }
-    for (let i = 0; i < document.querySelectorAll('img').length; i++) {
-        document.querySelectorAll('img')[i].classList.remove(removeClass);
+    for (let i = 0; i < $('img').length; i++) {
+        $('img')[i].classList.remove(removeClass);
     }
 }
 
